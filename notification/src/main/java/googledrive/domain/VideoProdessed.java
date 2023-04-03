6bba535f7a4460a0354cf2ee19a7c8716543b8a8
uengineYSW <112850933+uengineYSW@users.noreply.github.com>
2023-04-03 16:33:22 +0900

    commit

---
+++ notification/src/main/java/googledrive/domain/VideoProdessed.java
@@ -0,0 +1,15 @@
+package googledrive.domain;
+
+import googledrive.domain.*;
+import googledrive.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class VideoProdessed extends AbstractEvent {
+
+    private Long id;
+    private String fileid;
+    private String url;
+}
